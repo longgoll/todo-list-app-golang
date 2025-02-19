09eab4b77ec03565a5857b8d831b569fc57b02de
longgoll <luuhoanglong21@gmail.com>
2025-02-19 15:57:01 +0700

    add Swagger documentation and endpoints for todos API

--- todolist-api/cmd/main.go
+++ todolist-api/cmd/main.go
@@ -6,8 +6,18 @@ import (
 	"todolist-api/internal/repository"
 	"todolist-api/internal/service"
 
+	_ "todolist-api/docs" // Import the generated docs package
+
 	"github.com/gin-gonic/gin"
+	swaggerFiles "github.com/swaggo/files"
+	ginSwagger "github.com/swaggo/gin-swagger"
 )
+
+// @title Todo List API
+// @version 1.0
+// @description This is a simple Todo List application built with Golang and Gin framework.
+// @host localhost:8080
+// @BasePath /api/v1
 
 func main() {
 	r := gin.Default()
@@ -25,6 +35,9 @@ func main() {
 	v1.GET("/todos/:id", todoHandler.GetTodoByIDHandler)
 	v1.PUT("/todos/:id", todoHandler.UpdateTodoByIDHandler)
 	v1.DELETE("/todos/:id", todoHandler.DeleteTodoByIDHandler)
+
+	// Swagger endpoint
+	r.GET("/swagger/*any", ginSwagger.WrapHandler(swaggerFiles.Handler))
 
 	r.Run(":8080")
 }
--- todolist-api/internal/handlers/todo_handler.go
+++ todolist-api/internal/handlers/todo_handler.go
@@ -9,6 +9,11 @@ import (
 	"github.com/gin-gonic/gin"
 )
 
+// ErrorResponse represents the error response
+type ErrorResponse struct {
+	Error string `json:"error"`
+}
+
 type TodoHandler struct {
 	Service *service.TodoService
 }
@@ -17,6 +22,17 @@ func NewTodoHandler(service *service.TodoService) *TodoHandler {
 	return &TodoHandler{Service: service}
 }
 
+// CreateTodoHandler godoc
+// @Summary Create a new todo
+// @Description Create a new todo
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Param todo body models.TodoItem true "Todo Item"
+// @Success 201 {object} models.TodoItem
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /todos [post]
 func (h *TodoHandler) CreateTodoHandler(c *gin.Context) {
 	var todo models.TodoItem
 	if err := c.ShouldBindJSON(&todo); err != nil {
@@ -32,6 +48,14 @@ func (h *TodoHandler) CreateTodoHandler(c *gin.Context) {
 	c.JSON(http.StatusCreated, todo)
 }
 
+// GetTodosHandler godoc
+// @Summary Get all todos
+// @Description Get all todos
+// @Tags todos
+// @Produce json
+// @Success 200 {array} models.TodoItem
+// @Failure 500 {object} ErrorResponse
+// @Router /todos [get]
 func (h *TodoHandler) GetTodosHandler(c *gin.Context) {
 	todos, err := h.Service.GetAllTodos()
 	if err != nil {
@@ -42,6 +66,15 @@ func (h *TodoHandler) GetTodosHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, todos)
 }
 
+// GetTodoByIDHandler godoc
+// @Summary Get a todo by ID
+// @Description Get a todo by ID
+// @Tags todos
+// @Produce json
+// @Param id path string true "Todo ID"
+// @Success 200 {object} models.TodoItem
+// @Failure 404 {object} ErrorResponse
+// @Router /todos/{id} [get]
 func (h *TodoHandler) GetTodoByIDHandler(c *gin.Context) {
 	id := c.Param("id")
 	todo, err := h.Service.GetTodoByID(id)
@@ -53,6 +86,18 @@ func (h *TodoHandler) GetTodoByIDHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, todo)
 }
 
+// UpdateTodoByIDHandler godoc
+// @Summary Update a todo by ID
+// @Description Update a todo by ID
+// @Tags todos
+// @Accept json
+// @Produce json
+// @Param id path string true "Todo ID"
+// @Param todo body models.TodoItem true "Todo Item"
+// @Success 200 {object} models.TodoItem
+// @Failure 400 {object} ErrorResponse
+// @Failure 500 {object} ErrorResponse
+// @Router /todos/{id} [put]
 func (h *TodoHandler) UpdateTodoByIDHandler(c *gin.Context) {
 	id := c.Param("id")
 	var todo models.TodoItem
@@ -69,6 +114,15 @@ func (h *TodoHandler) UpdateTodoByIDHandler(c *gin.Context) {
 	c.JSON(http.StatusOK, todo)
 }
 
+// DeleteTodoByIDHandler godoc
+// @Summary Delete a todo by ID
+// @Description Delete a todo by ID
+// @Tags todos
+// @Produce json
+// @Param id path string true "Todo ID"
+// @Success 204
+// @Failure 500 {object} ErrorResponse
+// @Router /todos/{id} [delete]
 func (h *TodoHandler) DeleteTodoByIDHandler(c *gin.Context) {
 	id := c.Param("id")
 	if err := h.Service.DeleteTodoByID(id); err != nil {
